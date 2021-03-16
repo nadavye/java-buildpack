10af0b09ae6224e6b7a3332414054eb4c2d0c092
Nadav Yeheskel <nadav@sealights.io>
2021-03-16 20:30:57 +0200

    comment out download_uri

--- lib/java_buildpack/framework/sealights_agent.rb
+++ lib/java_buildpack/framework/sealights_agent.rb
@@ -45,7 +45,7 @@ module JavaBuildpack
         announce("1")
         download_url = get_download_url
         announce("2")
-        download_uri(download_url)
+        # download_uri(download_url)
         # announce("3")
         # extract_zip('sealights-java.zip', get_agent_path)
         # announce("4")
